05c355aa01a4ac27ac3e1d83f802c8b64c39b59e
Joshua Denenberg <j.denenberg42@gmail.com>
2017-09-28 23:56:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,15 @@
-require "pry"
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
-  def add_song(song)
-    @songs << song
-    song.artist(self)
+  def add_song_by_name(name)
+    song = Song.new(name, self)
   end
 
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
 end
